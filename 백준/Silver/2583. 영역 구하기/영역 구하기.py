52363ee060be59e18a9456915452e0d2e96f23f0
Yangdaehan <68599095+Yangdaehan@users.noreply.github.com>
2024-04-27 23:16:59 +0900

    [Silver I] Title: 영역 구하기, Time: 80 ms, Memory: 32588 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,40 +1,39 @@
-# BFS
-
-from collections import deque
+import sys
+sys.setrecursionlimit(10**6) # 재귀의 깊이 변경 (RecursionError)
 
 m, n, k = map(int, input().split())
 graph = [[0] * n for _ in range(m)]
 for _ in range(k):
     x1, y1, x2, y2 = map(int, input().split())
-    for i in range(x1, x2):
-        for j in range(m - y1 - 1, m - y2 - 1, -1):
-            graph[j][i] = 1
-
+    for i in range(y1, y2):
+        for j in range(x1, x2):
+            graph[i][j] = 1
+            
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
+count = 0
 
-def bfs(x, y):
-    global answer
-    queue = deque()
-    queue.append((x, y))
+def dfs(x, y):
+    global count
+    if x<0 or x>=m or y<0 or y>=n:
+        return 0
+    if graph[x][y] == 1:
+        return 0
+    
     graph[x][y] = 1
-    size = 1
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < m and 0 <= ny < n and graph[nx][ny] == 0:
-                graph[nx][ny] = 1
-                queue.append((nx, ny))
-                size += 1
-    result.append(size)
+    count += 1
+    for i in range(4):
+        dfs(x+dx[i], y+dy[i])
+    
+    return count
 
 result = []
 for i in range(m):
     for j in range(n):
-        if graph[i][j] == 0:
-            bfs(i, j)
+        cnt = dfs(i,j)
+        if cnt:
+            result.append(cnt)
+            count = 0
             
 result.sort()
 print(len(result))
